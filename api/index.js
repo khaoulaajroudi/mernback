c3f423a7d06145116776b4ee951dd3d9b051a9be
khaoula ajroudi <94904849+khaoulaajroudi@users.noreply.github.com>
2024-08-19 11:02:31 +0100

    first

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const dbconnect = require('./config/bdconnect')
+require('dotenv').config()
+const cors = require("cors");
+const app = express()
+dbconnect()
+
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+// middelware
+app.use(cors());
+app.use(express.json())
+app.use('/contact',require('./routes/contact'))
+const port=process.env.PORT
+app.listen(port,(err)=>err?console.log(err):console.log(`server is connected in ${port}`))
